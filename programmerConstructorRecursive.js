debbe8a7dc472e56c711b9ee2f6c0a4ad5744c40
Whitley Blaine <whitleyblaine@gmail.com>
2016-07-12 15:52:08 -0400

    Created recursive programer constructor

---
+++ programmerConstructorRecursive.js
@@ -0,0 +1,40 @@
+var inquirer = require('inquirer');
+
+var Programmer = function(options) {
+  this.name = options.name;
+  this.title = options.title;
+  this.age = options.age;
+  this.language = options.language;
+  this.print = function() {
+    console.log(this.name, this.title, this.age, this.language);
+  };
+}
+
+var count = 0;
+
+var askQuestion = function() {
+  if (count < 5) {
+    inquirer.prompt([{
+      name: "name",
+      message: "What is your name?"
+    }, {
+      name: "title",
+      message: "What is your title?"
+    }, {
+      name: "age",
+      message: "What is your age?"
+    }, {
+      name: "language",
+      message: "What is your favourite programming language?"
+    }]).then(function(answers) {
+      var newGuy = new Programmer({name: answers.name, title: answers.title, age: answers.age, language: answers.language});
+      newGuy.print();
+      count++;
+      askQuestion();
+    })
+  } else {
+    console.log("All questions asked");
+  }
+}
+
+askQuestion();
