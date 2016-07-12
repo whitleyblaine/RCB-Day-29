d3619fa5642ea14c26f56e4906c9d39a32b4a732
Whitley Blaine <whitleyblaine@gmail.com>
2016-07-12 15:22:53 -0400

    Updated programmer constructor to use inquirer

--- programmerConstructor.js
+++ programmerConstructor.js
@@ -1,13 +1,28 @@
+var inquirer = require('inquirer');
+
 var Programmer = function(options) {
   this.name = options.name;
   this.title = options.title;
   this.age = options.age;
-  this.lang = options.lang;
+  this.language = options.language;
   this.print = function() {
-    console.log(this.name, this.title, this.age, this.lang);
+    console.log(this.name, this.title, this.age, this.language);
   };
 }
 
-var Whitley = new Programmer({name: 'Whitley', title: 'Bitcoin Dev', age: 22, lang: 'Javascript'});
-
-Whitley.print();
+inquirer.prompt([{
+  name: "name",
+  message: "What is your name?"
+}, {
+  name: "title",
+  message: "What is your title?"
+}, {
+  name: "age",
+  message: "What is your age?"
+}, {
+  name: "language",
+  message: "What is your favourite programming language?"
+}]).then(function(answers) {
+  var newGuy = new Programmer({name: answers.name, title: answers.title, age: answers.age, language: answers.language});
+  newGuy.print();
+})
